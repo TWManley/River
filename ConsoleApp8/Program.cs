085f355c3591fc17435e9824a0f36be4e5f6c2db
twmanley <tmanley1@yahoo.com>
2018-06-01 13:47:40 -0400

    first commit

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the name of the first river?");
+            string riverOne = Console.ReadLine();
+            Console.WriteLine("what is the name of the second river?");
+            string riverTwo = Console.ReadLine();
+            string userInput = "yes";
+            River ohio = new River(riverOne);
+            River Lake = new River(riverTwo);
+            
+            Console.WriteLine("The {0} river has a pollution level of {1} and a depth of {2}", ohio.Name, ohio.Pollution, ohio.Depth);
+            Console.WriteLine("The {0} river has a pollution level of {1} and a depth of {2}", Lake.Name, Lake.Pollution, Lake.Depth);
+
+            {
+               
+
+
+                Console.WriteLine("Which river would you like to manage? or type quit to quit");
+                string rivername = Console.ReadLine().ToLower();
+                if (rivername == riverOne)
+                {
+                    do
+                    {
+                      
+                        Console.WriteLine("What would you like to do. Options include: riverstatus, riverdepth, cleanriver, drainriver, depthinput, riverpollution, return,  or help for more information");
+                        userInput = Console.ReadLine();
+                        if (userInput == "riverdepth")
+                        {
+                            ohio.LookDepth();
+                        }
+                        else if (userInput == "riverstatus")
+                        {
+                            ohio.Look();
+                        }
+                        else if (userInput == "cleanriver")
+                        {
+                            ohio.CleanRiver();
+                            ohio.LookPollution();
+                        }
+                        else if (userInput == "drainriver")
+                        {
+                            ohio.Drain();
+                            ohio.LookDepth();
+                        }
+                        else if (userInput == "help")
+                            {
+                            Console.WriteLine();
+                                Console.WriteLine("riverstatus checks the pollution and depth levels for the river?");
+                                Console.WriteLine("river depth checks the depth of the river");
+                                Console.WriteLine("cleanriver is a reduction of the pollution level");
+                                Console.WriteLine("drainriver will reduce the water level");
+                                Console.WriteLine("depthinput allows you to input the rivers depth");
+                                Console.WriteLine("riverpollution shows the pollution level of the river");
+                                Console.WriteLine("return will return you to lake selection");
+                            Console.WriteLine();
+                            }
+                        else if (userInput == "riverpollution")
+                        {
+                            ohio.LookPollution();
+
+                        }
+                        else if (userInput == "depthinput")
+                        {
+                            ohio.RiverDepth();
+
+                        }
+                    }
+                    while (userInput != "return" );
+
+                        
+
+                }
+                if (rivername == riverTwo)
+                {
+                    do
+                    {
+                    
+                        Console.WriteLine("What would you like to do. Options include: riverstatus, riverdepth, cleanriver, drainriver, depthinput, riverpollution, or to go back type return");
+                        userInput = Console.ReadLine();
+                        if (userInput == "riverdepth")
+                        {
+                           Lake.LookDepth();
+                        }
+                        else if (userInput == "riverstatus")
+                        {
+                            Lake.Look();
+                        }
+                        else if (userInput == "cleanriver")
+                        {
+
+                            Lake.CleanRiver();
+                            Lake.LookPollution();
+                        }
+                        else if (userInput == "drainriver")
+                        {
+                            Lake.Drain();
+                            Lake.LookDepth();
+                        }
+                            else if (userInput == "help")
+                            {
+                            Console.WriteLine();
+                                Console.WriteLine("riverstatus checks the pollution and depth levels for the river?");
+                                Console.WriteLine("river depth checks the depth of the river");
+                                Console.WriteLine("cleanriver is a reduction of the pollution level");
+                                Console.WriteLine("drainriver will reduce the water level");
+                                Console.WriteLine("depthinput allows you to input the rivers depth");
+                                Console.WriteLine("riverpollution shows the pollution level of the river");
+                                Console.WriteLine("return will return you to lake selection");
+
+                            Console.WriteLine();
+
+                        }
+                        else if (userInput == "riverpollution")
+                        {
+                            Lake.LookPollution();
+
+                        }
+                        else if (userInput == "depthinput")
+                        {
+                            Lake.RiverDepth();
+
+                        }
+                        
+                    }
+                    while (userInput != "return");
+                        if (rivername == "quit")
+                        {
+                            return;
+                        }
+
+
+
+
+                 
+
+               
+             
+
+
+                    
+                }
+        }
+
+    }
+    }
+}
---
+++ ConsoleApp8/River.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitor
+{
+    class River
+    {
+        //fields
+        public string name;
+        public int depth;
+        public int pollution;
+
+
+        // properties
+        public int Depth
+        {
+            get { return this.depth; }
+            set { this.depth = value; }
+
+        }
+
+        public int Pollution
+        {
+            get { return this.pollution; }
+            set { this.pollution = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public River()
+        {
+            //default constructor
+        }
+        public River(string nameOne) // loaded constructor for the class that creates a class with a name input then assigns the pollution and depth of the clas to 100 and 20 respectivly
+        {
+            this.name = nameOne;
+            this.pollution = 100;
+            this.depth = 20;
+        }
+        //methods
+        public void Look() //adds a random number to a given lake and displays the depth and pollution
+        {
+            Random randomGen = new Random();
+            int randomDepth = randomGen.Next(1, 11);
+            int randomPollution = randomGen.Next(1, 101);
+            this.depth += randomDepth;
+            this.pollution += randomPollution;
+            Console.WriteLine("The {0} river has depth of {1} ft and a pollutoin level of {2}", this.Name, this.depth, this.pollution);
+            WarningCheck(this.depth, this.pollution);
+        }
+        public void RiverDepth() // sets the depth of the river to a given user input
+        {
+            Console.WriteLine("what is the depth of the river?");
+            this.depth = int.Parse(Console.ReadLine());
+            WarningCheck(this.depth, this.pollution);
+
+        }
+        public void CleanRiver() // sets the pollution field equal to zero
+        {
+            this.pollution = 0;
+        }
+        public void LookDepth() // displays the depth field
+        {
+            Console.WriteLine("The {0} river has a depth of {1}", this.name, this.depth);
+            WarningCheck(this.depth, this.pollution);
+        }
+        public void LookPollution() // displays the pollution field
+        {
+            
+            Console.WriteLine("The {0} river has a pollution level of {1} ", this.name, this.pollution);
+            WarningCheck(this.depth, this.pollution);
+        }
+        public void WarningCheck(int depthtest, int pollutiontest) // method used throughout this class that displays a warning if the depth or the pollution exceeds a value
+        {
+            string addWater;
+            if (depthtest > 40) 
+
+            {
+                Console.WriteLine("The river has exceeded above acceptable depth levels");
+
+            }
+            if (pollutiontest > 1000)
+            {
+                Console.WriteLine("The river is too polluted");
+
+            }
+            if (depthtest < 10)
+            {
+                Console.WriteLine("Would you like to add water?");
+                addWater= Console.ReadLine();
+                if (addWater == "yes")
+                {
+                    this.depth = 20;
+                }
+            }
+      
+           
+        }
+        public void Drain() // sets the depth field equal to zero
+        {
+            this.depth = 0;
+        }
+        
+
+
+
+
+
+
+
+    }
+
+}
+
+
